6cf5e9767e87efda1aa942cfb7b7ca94baa0a10c
chewbakai <kyrahceniza@gmail.com>
2021-11-15 21:06:05 +0800

    updated version

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,9 @@ import 'package:get/get.dart';
 import './screens/LoginScreen.dart';
 import './routes.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 void main() {
   runApp(GetMaterialApp(
     theme: ThemeData(
@@ -19,4 +22,37 @@ void main() {
     getPages: routes,
     initialRoute: LoginScreen.routeName,
   ));
+}
+class App extends StatefulWidget {
+  // Create the initialization Future outside of `build`:
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  /// The future is part of the state of our widget. We should not call `initializeApp`
+  /// directly inside [build].
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return SomethingWentWrong();
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MyAwesomeApp();
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return Loading();
+      },
+    );
+  }
 }
